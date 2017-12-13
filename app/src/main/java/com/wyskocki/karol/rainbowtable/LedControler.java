3ae826d7e2342f138d46a829396f9bdf7f70932c
Karol Wyskocki <karol.wyskocki@vp.pl>
2017-12-13 01:41:40 +0100

    BT communication

---
+++ app/src/main/java/com/wyskocki/karol/rainbowtable/LedControler.java
@@ -0,0 +1,82 @@
+package com.wyskocki.karol.rainbowtable;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.graphics.Color;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
+
+/**
+ * Created by karol on 12.12.17.
+ */
+
+public class LedControler {
+
+    //instruction
+    private final int SEND_COLOR = 1;
+    private final int SEND_RAINBOW = 82;
+
+    private static UUID PRIVATE_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    private BluetoothDevice btDevice;
+    private BluetoothSocket socket;
+    private OutputStream outStream;
+
+
+    public LedControler(BluetoothDevice device){
+        this.btDevice = device;
+    }
+
+    public void sendColor(int color) throws IOException {
+        byte red = (byte) Color.red(color);
+        byte green = (byte) Color.green(color);
+        byte blue = (byte) Color.blue(color);
+        send(new byte[]{SEND_COLOR, red, green, blue});
+    }
+
+    public void sendAnimation(int speed, int amplitude) throws IOException {
+        send(new byte[]{SEND_RAINBOW, (byte)speed, (byte)amplitude});
+    }
+
+
+    private void send(byte[] frame) throws IOException {
+        if(isConnected()){
+            outStream.write(frame);
+            outStream.flush();
+        }
+    }
+
+    public void connect() throws IOException {
+        BluetoothSocket tmp = null;
+        tmp = btDevice.createRfcommSocketToServiceRecord(PRIVATE_UUID);
+        socket = tmp;
+
+        Thread connectionThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    socket.connect();
+                } catch (IOException e) {
+                    try {
+                        socket.close();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        });
+        connectionThread.start();
+        outStream = socket.getOutputStream();
+    }
+
+    public boolean isConnected(){
+        return socket.isConnected();
+    }
+
+    public void close() throws IOException {
+            outStream.close();
+            socket.close();
+    }
+}
